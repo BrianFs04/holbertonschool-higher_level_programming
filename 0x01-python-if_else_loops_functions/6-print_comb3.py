386e9e4c471505ad9dc766ab475830f834923086
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-10 16:55:14 +0000

    [Feature]: Prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for numbers in range(0, 10):
+    for repeated_numbers in range(0, 10):
+        if numbers < repeated_numbers:
+            if numbers == 8 and repeated_numbers == 9:
+                print('{}{}'.format(numbers, repeated_numbers), end = '')
+            else:
+                print('{}{}, '.format(numbers, repeated_numbers), end = '')
+print('')
