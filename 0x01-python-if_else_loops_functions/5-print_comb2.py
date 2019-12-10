271f7c57199d13ca279515e0b3ba8bb2bfad9e69
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-10 17:28:25 +0000

    [Update]: Adding new line at the end

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for numbers in range(00, 100):
     if numbers >= 0 and numbers <= 98:
         print('{:02d}, '.format(numbers), end='')
     else:
-        print('{}'.format(numbers), end='')
+        print('{}'.format(numbers))
