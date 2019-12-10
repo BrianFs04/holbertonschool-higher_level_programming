91b497e3ea48a1db1847a4547172ea97deb49792
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-10 17:03:17 +0000

    [Update]: Fix typo and adding abs function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = str(number)
-if int(last[-1:]) > 5:
-    print('Last digit of {:d} is {:s} and is greater than 5'.format(number, last[-1:]))
-elif int(last[-1:]) == 0:
-    print('Last digit of {:d} is {:s} and is 0'.format(number, last[-1:]))
+last = abs(number) % 10
+if last > 5:
+    print('Last digit of {} is {} and is greater than 5'.format(number, last))
+elif last == 0:
+    print('Last digit of {} is {} and is 0'.format(number, last))
 else:
-    print('Last digit of {:d} is {:s} and is less than 6 and not 0'.format(number, last[-1:]))
+    print('Last digit of {} is {} and is less than 6 and not 0'
+          .format(number, last))
