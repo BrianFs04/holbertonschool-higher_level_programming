d332c3895df2baf383780749c34510605bbb644d
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-10 20:45:36 +0000

    [Update]: Deleting conditions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        c = ord(i)
+    a = ''
+    for i in range(len(str)):
+        c = ord(str[i])
         if c >= 97 and c <= 122:
             a = chr(c - 32)
         elif c >= 65 and c <= 90:
             a = chr(c)
-        elif c >= 48 and c <= 57:
-            a = chr(c)
-        elif c == 32:
-            a = ' '
         else:
-            a = ''
+            a = chr(c)
         print('{}'.format(a), end='')
     print('')
