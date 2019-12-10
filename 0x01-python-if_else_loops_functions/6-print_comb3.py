398ae3d0f330ffbf91d00144613427b2b29bc38b
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-10 17:29:27 +0000

    [Fix]: Typo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for numbers in range(0, 10):
     for repeated_numbers in range(0, 10):
         if numbers < repeated_numbers:
             if numbers == 8 and repeated_numbers == 9:
-                print('{}{}'.format(numbers, repeated_numbers), end = '')
+                print('{}{}'.format(numbers, repeated_numbers), end='')
             else:
-                print('{}{}, '.format(numbers, repeated_numbers), end = '')
+                print('{}{}, '.format(numbers, repeated_numbers), end='')
 print('')
