0b730234aae7cd470c98a9c3480bd7fb74855e4e
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-17 22:22:47 +0000

    [Fix]: Using sort function

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,5 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    else:
-        n = 0
-        for i in my_list:
-            if i > n:
-                n = i
-    return(n)
+    my_list.sort()
+    return my_list[-1]
