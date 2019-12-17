730b39036bb3df8dbf958b62272735faa9786b42
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-17 21:55:33 +0000

    [Fix]: Typo

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     if not tuple_b:
         tuple_b = (0, 0)
-    if len(tuple_a) >= 0 and len(tuple_a) < 1:
+    if len(tuple_a) >= 0 and len(tuple_a) < 2:
         tuple_a = (tuple_a[0], 0)
     if len(tuple_b) >= 0 and len(tuple_b) < 2:
         tuple_b = (tuple_b[0], 0)
