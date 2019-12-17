3aeda701ebb4917e541ea3dd6ae8ba382c0ec1c9
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-17 19:40:44 +0000

    [Add]: Integer format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for rows in range(0, len(matrix)):
         for elems in range(0, len(matrix[rows])):
             if elems < len(matrix[rows]) - 1:
-                print('{}'.format(matrix[rows][elems]), end=' ')
+                print('{:d}'.format(matrix[rows][elems]), end=' ')
             else:
-                print('{}'.format(matrix[rows][elems]))
+                print('{:d}'.format(matrix[rows][elems]))
         if not matrix[rows]:
             print('')
