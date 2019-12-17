d3a4a671184a58d6c7a2a444afa7f5692da7d4b9
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-17 18:31:52 +0000

    [Fix]: Condition in which the list is void

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list)
+    if not my_list:
+        pass
     while i > 0:
         print('{:d}'.format(i), end='\n')
         i -= 1
