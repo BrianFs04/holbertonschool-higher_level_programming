c1725f81550bd156ceb4d7dc81fe0ef765ef22ba
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-19 20:27:34 +0000

    [Fix]: Typo

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     for key in a_dictionary:
-        if key == '' in a_dictionary:
-            return ''
-    return sorted(a_dictionary.keys())[-1]
+        if key not in a_dictionary:
+            return None
+        else:
+            return sorted(a_dictionary.keys())[-1]
