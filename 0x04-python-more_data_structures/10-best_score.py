0f28b1cb5a5899838db472733515ffbbf1abd965
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-19 20:25:26 +0000

    [Add]: Condition in whcih the key is void

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
+    for key in a_dictionary:
+        if key == '' in a_dictionary:
+            return ''
     return sorted(a_dictionary.keys())[-1]
