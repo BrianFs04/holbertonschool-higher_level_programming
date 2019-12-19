ac7ea70abe1979a4e5582deb8419692e73582b38
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-19 23:54:02 +0000

    [Add]: Returns 0 if the list is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     sums = 0
     sums2 = 0
     score = [[first*second for first, second in zip(elem, elem[1:])]
