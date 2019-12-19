773683b695114e084e0f7560182e655982284c6a
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-19 19:22:09 +0000

    [Feature]: Function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for keys, values in a_dictionary.items():
+        if key == keys:
+            a_dictionary[keys] = value
+    a_dictionary[key] = value
+    return a_dictionary
