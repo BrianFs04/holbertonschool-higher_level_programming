610f449c9f1519187862fd798f283a9a717bd7c9
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-19 20:34:54 +0000

    [Delete]: Useful file

--- 0x04-python-more_data_structures/10-best_score.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    for key in a_dictionary:
-        if key not in a_dictionary:
-            return None
-        else:
-            return sorted(a_dictionary.keys())[-1]
