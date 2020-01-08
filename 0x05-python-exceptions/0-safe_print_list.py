e7a35b05308291758ea757a7a8576b145e885eb5
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-08 18:04:49 +0000

    [Update]: Adding error in except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list[:x]:
             print("{}".format(i), end='')
-    except:
+    except IndexError:
         pass
     finally:
         print('')
