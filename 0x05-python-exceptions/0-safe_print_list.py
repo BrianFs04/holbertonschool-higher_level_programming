410f682c272056751ce96e17f2e01fce45b98a5e
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-08 19:41:05 +0000

    [Add]: Counting variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    ind = 0
     try:
-        for i in my_list[:x]:
-            print("{}".format(i), end='')
+        for i in range(0, x):
+            print("{}".format(my_list[ind]), end='')
+            ind += 1
     except IndexError:
         pass
     finally:
         print('')
-        return i
+        return ind
