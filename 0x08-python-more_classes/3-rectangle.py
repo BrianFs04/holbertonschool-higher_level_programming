f94148631ef74d471a71bb00680d754fc820755f
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-15 03:03:30 +0000

    [Fix]: Changing from private attribute to public in order to print error messages

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -5,8 +5,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         '''Constructor
         '''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def __str__(self):
         '''Str method
--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         '''Constructor
         '''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
