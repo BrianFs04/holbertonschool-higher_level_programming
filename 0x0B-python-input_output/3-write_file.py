7a5e644e9d538287262bc5ac42a0b2f75d05851a
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-21 23:51:20 +0000

    [Delete]: Trash

--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def write_file(filename="", text=""):
-    '''Writes a string to a text file
-    '''
-    with open(filename, 'w+') as f:
-        add_s = f.write('Holberton School is so cool!\n')
-    return add_s
