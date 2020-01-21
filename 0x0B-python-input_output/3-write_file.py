32265f216268e5e59f4461ccc6cc41560ba92364
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-21 23:49:03 +0000

    [Fix]: Adding + to write parameter

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -2,6 +2,6 @@
 def write_file(filename="", text=""):
     '''Writes a string to a text file
     '''
-    with open(filename, 'w') as f:
+    with open(filename, 'w+') as f:
         add_s = f.write('Holberton School is so cool!\n')
     return add_s
