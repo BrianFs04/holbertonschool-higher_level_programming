47d82fa8bffaabc69bd3b56debeb8f898d9bde1d
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-27 18:25:41 +0000

    [Feature]: Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    ''' Square class
+    '''
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,
+                                                 self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        listy = ['id', 'size', 'x', 'y']
+        if args is not None and len(args) is not 0:
+            for i in range(len(args)):
+                setattr(self, listy[i], args[i])
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    def to_dictionary(self):
+        dicty = {'id': self.id, 'size': self.size, 'x': self.x,
+                 'y': self.y}
+        return dicty
