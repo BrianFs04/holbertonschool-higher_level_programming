16b1a3d1bf7d8b915638ba9660fee0bdcbba87bf
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-27 19:04:35 +0000

    [Fix]: Adding comments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,13 @@ class Square(Rectangle):
     ''' Square class
     '''
     def __init__(self, size, x=0, y=0, id=None):
+        ''' Constructor
+        '''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        ''' Str method
+        '''
         return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,
                                                  self.width)
 
