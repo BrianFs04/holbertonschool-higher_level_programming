e9c4dcc72fa0104d06256cb66fd391727af3b5d8
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-27 19:32:42 +0000

    [Fix]: Typo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,9 +50,9 @@ class Base:
     def create(cls, **dictionary):
         ''' Returns an instance with all attributes
         '''
-        if cls.__name__ is Rectangle:
+        if cls.__name__ is 'Rectangle':
             dummy = cls(1, 1)
-        if cls.__name__ is Square:
+        if cls.__name__ is 'Square':
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
