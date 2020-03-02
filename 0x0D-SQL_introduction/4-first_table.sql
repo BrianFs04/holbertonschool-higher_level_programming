1b9b9b661690c673fe8719bc70cfcd6abcebf337
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-02 19:39:38 +0000

    [Feature]: Creates a table called first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
--- Creates a table called first_table in the current database
+-- Creates a table called first_table in the database
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
