915e9573a0d60861109ad4a39af04845bc32b32d
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-03 20:18:35 +0000

    [Feature]: Creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0d_usa and the table states
+-- Create a primary key auto generated
+CREATE DATABASE hbtn_0d_usa;
+CREATE TABLE states (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       name VARCHAR(256))
