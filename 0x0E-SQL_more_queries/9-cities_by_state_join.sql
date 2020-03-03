090925a398f58e69f95ef849e8fd976a0ac15827
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-03 22:27:22 +0000

    [Fix]: Typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- Each record should display: cities.id - cities.name - states.name
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id = state_id;
+INNER JOIN states ON cities.state_id = states.id;
