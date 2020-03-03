9a2ae201fa9370b2e545a064898ea30f7d730643
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-03 22:25:55 +0000

    [Fix]: Changing order

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- Lists all cities contained in the database hbtn_0d_usa
 -- Each record should display: cities.id - cities.name - states.name
 SELECT cities.id, cities.name, states.name
-FROM cities INNER JOIN states ON state_id = cities.state_id;
+FROM cities
+INNER JOIN states ON cities.state_id = state_id;
