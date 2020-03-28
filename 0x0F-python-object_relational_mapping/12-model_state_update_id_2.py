f8e9010e0769f4444f1385f7d96840862efc3983
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-28 19:32:58 +0000

    [Feature]: Script that changes the name of a State object from the database hbtn_0e_6_usa via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Script that changes the name of a State object from the
+database hbtn_0e_6_usa via SQLAlchemy
+'''
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.id == 2).first()
+    states.name = "New Mexico"
+    session.commit()
