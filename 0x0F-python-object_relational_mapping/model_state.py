2203b5c7ef64e484e0307ac711d81496cc034a1a
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-28 19:23:36 +0000

    [Fix]: Pep8 style

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,13 +3,13 @@
 File that contains the class definition of a State and an
 instance Base = declarative_base()
 '''
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, unique = True, nullable = False, primary_key=True)
-    name =  Column(String(128), nullable = False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
