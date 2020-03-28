907e77a64d319d2a351170dceae3ec0b4f15bb40
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-28 19:15:24 +0000

    [Feature]: Displays all values in the states table of hbtn_0e_0_usa where name matches the last argument, safe from MySQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Script that takes in arguments and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument. But this time, write one that is
+safe from MySQL injections
+'''
+import MySQLdb
+import sys
+
+
+def mysqlconnect():
+    db_connection = None
+    db_connection = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4], ))
+    states = cursor.fetchone()
+
+    print(states)
+
+    cursor.close()
+    db_connection.close()
+
+if __name__ == '__main__':
+    mysqlconnect()
