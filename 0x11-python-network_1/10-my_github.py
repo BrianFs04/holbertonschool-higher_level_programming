5db89d56aa4032fb537e9f93e933ecb4a3bc9139
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-15 21:45:48 +0000

    [Fix]: Typo an pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,6 @@ import sys
 
 if __name__ == '__main__':
 
-    x = requests.get('https://api.github.com/', auth=(sys.argv[1],
-                                                       sys.argv[2]))
-
+    x = requests.get('https://api.github.com/user', auth=(sys.argv[1],
+                                                          sys.argv[2]))
     print(x.json().get('id'))
