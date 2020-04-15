1db2de7dd3ce42862199a2f310cecf9e43f264bf
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-15 21:40:13 +0000

    [Feature]: Conection to GitHub API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    x = requests.get('https://api.github.com/', auth=(sys.argv[1],
+                                                       sys.argv[2]))
+    print(x.json().get('id'))
