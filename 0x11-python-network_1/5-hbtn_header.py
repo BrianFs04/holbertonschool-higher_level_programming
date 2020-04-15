cc17835dbecb02cfbe4d9f9110b801a6533371fc
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-15 19:23:10 +0000

    [Feature]: Sends a request to the URL and displays the value of the variable X-Request-Id, using requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Sends a request to the URL and displays the value of the
+   X-Request-Id variable found in the header
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    x = requests.get(sys.argv[1])
+    print(x.headers['X-Request-Id'])
