bf1774c35cb50a6eb73aa5ecafd006da13009c89
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-05 18:32:12 +0000

    [Fix]: semistandard style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 let myVar;
 
 if (myVar === process.argv[2]) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (myVar === process.argv[3]) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
