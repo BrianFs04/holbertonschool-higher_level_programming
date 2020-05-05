2fef7bdb3b42224535803de012347fbe961950e4
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-05 19:21:00 +0000

    [Fix]: semistandard style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const myVar = process.argv[2];
 
 if (Number(myVar)) {
-  console.log("My number: " + parseInt(myVar));
+  console.log('My number: ' + parseInt(myVar));
 } else {
   console.log('Not a number');
 }
