b3cef6bfb0656748303875ff9aa09d48db5ebcbd
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-11 21:57:02 +0000

    [Fix]: Prints the param passed to it

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,12 @@
 const Squarel = require('./5-square.js');
 
 module.exports = class Square extends Squarel {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     for (let i = 0; i < this.height; i++) {
       if (c === undefined) {
         console.log('X'.repeat(this.width));
       } else {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
