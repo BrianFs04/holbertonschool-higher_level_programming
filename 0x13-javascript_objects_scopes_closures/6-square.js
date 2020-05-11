3ee45bc85d91e7c3b91c97675526af394400bc69
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-11 21:53:50 +0000

    [Feature]: Double inheritance

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Squarel = require('./5-square.js');
+
+module.exports = class Square extends Squarel {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+};
