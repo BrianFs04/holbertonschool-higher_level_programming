537a60ec69dfe9e5a5173563ec9e27466608813e
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-12 22:51:29 +0000

    [Features]: List of characters in a movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    for (const i in info.characters) {
+      request(info.characters[i], function (error, respose, body) {
+        if (!error && response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+}
+
+request(options, callback);
