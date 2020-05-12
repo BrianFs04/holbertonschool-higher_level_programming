691cbe7a2389640dea6ab39ac205e0a330510627
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-12 21:07:44 +0000

    [Feature]: Gets the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const options = {
+  url: `${process.argv[2]}`,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFileSync(process.argv[3], body, 'utf8')
+  }
+}
+
+request(options, callback);
