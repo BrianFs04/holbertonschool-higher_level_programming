4d05608588a7492c79536f07f9ed404269aa6688
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-12 20:11:19 +0000

    [Fix]: Semistandard style

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,8 +9,8 @@ const options = {
   }
 };
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
     const info = JSON.parse(body);
     console.log(info.title);
   }
