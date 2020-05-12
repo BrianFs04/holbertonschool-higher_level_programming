c8f3b480290fc6a7fee0459faef74c4bf71b0d04
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-12 20:15:14 +0000

    [Fix]: URL

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/',
   headers: {
     'User-Agent': 'request'
   }
