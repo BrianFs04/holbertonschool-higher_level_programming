6c28d943fc1cd20c0f93104a43d8fa4ecee81cbf
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-13 01:06:41 +0000

    [Trash]: This should be fixed to print the right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+const newList = [];
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    for (const i in info.characters) {
+      request(info.characters[i], function (error, respose, body) {
+        if (!error && response.statusCode === 200) {
+          const chars = JSON.parse(body);
+          console.log(chars.name);
+        }
+      });
+    }
+  }
+}
+
+request(options, callback);
