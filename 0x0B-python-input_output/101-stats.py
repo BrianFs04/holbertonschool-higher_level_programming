7920ba1be5bb7e2e1f040a31259105e9691bb646
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-28 18:35:09 -0500

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,29 +4,27 @@ import signal
 import sys
 
 
-def signal_handler(sig, frame):
-    """Handles Ctrl+C"""
-    print("File size: {}".format(size))
-    for k, v in sorted(ordered.items()):
-        print("{}: {}".format(k, v))
-    sys.exit(0)
-
 if __name__ == "__main__":
     ordered = {}
     size = i = 0
     count = 1
     data = sys.stdin
-    for line in data:
-        i += 1
-        item = line.split(" ")
-        st_code = int(item[-2])
-        size += int(item[-1])
-        if ordered.get(st_code):
-            ordered[st_code] += count
-        else:
-            ordered[st_code] = count
-        if i % 10 == 0:
-            print("File size: {}".format(size))
-            for k, v in sorted(ordered.items()):
-                print("{}: {}".format(k, v))
-        signal.signal(signal.SIGINT, signal_handler)
+    try:
+        for line in data:
+            i += 1
+            item = line.split(" ")
+            st_code = int(item[-2])
+            size += int(item[-1])
+            if ordered.get(st_code):
+                ordered[st_code] += count
+            else:
+                ordered[st_code] = count
+            if i % 10 == 0:
+                print("File size: {}".format(size))
+                for k, v in sorted(ordered.items()):
+                    print("{}: {}".format(k, v))
+    except KeyboardInterrupt:
+        print("File size: {}".format(size))
+        for k, v in sorted(ordered.items()):
+            print("{}: {}".format(k, v))
+        raise
