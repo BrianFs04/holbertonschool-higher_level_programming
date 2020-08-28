9628576ac64b63ef88ac172357ddb854a1686f7e
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-28 18:00:03 -0500

    [Feature]: Reads stdin line by line and computes metrics

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics"""
+import signal
+import sys
+
+
+def signal_handler(sig, frame):
+    """Handles Ctrl+C"""
+    print("File size {}".format(size))
+    for k, v in sorted(ordered.items(), reverse=True):
+        print(k, v)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    ordered = {}
+    size = i = 0
+    count = 1
+    data = sys.stdin
+    for line in data:
+        i += 1
+        item = line.split(" ")
+        st_code = int(item[-2])
+        size += int(item[-1])
+        if ordered.get(st_code):
+            ordered[st_code] += count
+        else:
+            ordered[st_code] = count
+        if i % 10 == 0:
+            print("File size {}".format(size))
+            for k, v in sorted(ordered.items(), reverse=True):
+                print(k, v)
+        signal.signal(signal.SIGINT, signal_handler)
